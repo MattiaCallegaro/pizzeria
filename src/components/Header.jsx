a800f1a555592643adb726fbe07f054b333317f1
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-06-27 15:46:44 +0200

    [UPD] cambiato logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <NavLink className="navbar-brand mx-3 " to="/"> <img src="/vite.svg" alt="" /></NavLink>
+    <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#F1E1C7' }}>
       <div className="container-fluid d-flex flex-column align-items-center">
         <div className="d-flex justify-content-center align-items-center w-100">
-
-          <NavLink className="navbar-brand mx-3 " to="/"> <h3>Pizzeria Grande Piramide D'Egitto</h3></NavLink>
+          <NavLink className="navbar-brand mx-3 " to="/"> <img src="\pizza-logo.png"
+            alt="logo"
+            className="d-block mx-auto"
+            style={{ width: '15%', height: 'auto' }} /></NavLink>
           <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
